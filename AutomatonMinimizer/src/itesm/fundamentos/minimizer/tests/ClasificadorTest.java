818b4c3e5206688a851c0ad9b0864845f9a755bd
xico.delvalle <xico.delvalle@gmail.com>
2011-11-16 06:47:12 +0000

    Agregando primer ejemplo para data driven testing.

--- AutomatonMinimizer/src/itesm/fundamentos/minimizer/tests/ClasificadorTest.java
+++ AutomatonMinimizer/src/itesm/fundamentos/minimizer/tests/ClasificadorTest.java
@@ -1,14 +1,44 @@
 package itesm.fundamentos.minimizer.tests;
 
+import itesm.fundamentos.minimizer.control.Clasificador;
+import itesm.fundamentos.minimizer.modelo.*;
+
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class ClasificadorTest {
+	
+	private Automata inputAutomata = null;
+	private boolean esDeterminista = false;
+	
+	@Parameters
+    public static Collection spreadsheetData() throws IOException {
+        return new AutomataBuilder("c:/temp/testdata.xls").getData();
+    }
+
+	public ClasificadorTest(Automata inputAutomata, boolean esDeterminista)
+	{
+		this.inputAutomata = inputAutomata;
+		this.esDeterminista = esDeterminista;
+	}
 
 	@Test
 	public void testEsDeterminista() {
-		fail("Not yet implemented");
+		boolean resultado = Clasificador.EsDeterminista(inputAutomata);
+		assertSame(resultado, esDeterminista);
 	}
+	
+	
 
 }
