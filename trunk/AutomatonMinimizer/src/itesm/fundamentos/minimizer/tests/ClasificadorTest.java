741e2ca015e86b060df1e8476c9eb1cee5d1eb5c
xico.delvalle@gmail.com <xico.delvalle@gmail.com@9239d40f-f9b8-a7cb-b866-32f055a43c66>
2011-11-16 05:48:29 +0000

    - Added initial skeleton for ClasificadorTest

---
+++ trunk/AutomatonMinimizer/src/itesm/fundamentos/minimizer/tests/ClasificadorTest.java
@@ -0,0 +1,14 @@
+package itesm.fundamentos.minimizer.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClasificadorTest {
+
+	@Test
+	public void testEsDeterminista() {
+		fail("Not yet implemented");
+	}
+
+}
