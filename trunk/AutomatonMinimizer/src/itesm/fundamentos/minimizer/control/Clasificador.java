f8a9658682e099e62cdbd0e6889c22fe7dfaef92
rejeshua@gmail.com <rejeshua@gmail.com@9239d40f-f9b8-a7cb-b866-32f055a43c66>
2011-11-16 01:37:26 +0000

    Agregando funcion clasificador

--- trunk/AutomatonMinimizer/src/itesm/fundamentos/minimizer/control/Clasificador.java
+++ trunk/AutomatonMinimizer/src/itesm/fundamentos/minimizer/control/Clasificador.java
@@ -1,11 +1,131 @@
 package itesm.fundamentos.minimizer.control;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import itesm.fundamentos.minimizer.modelo.*;
 
 public class Clasificador {
 	
+	//
+	// Reglas para considerar que un automata es determinista
+	//  - tiene una transicion para cada simbolo del alfabeto.
+	// 	- no transiciones vacias
+	//  - con un mismo simbolo del alfabeto no puede existir dos transiciones
+	//
 	public static boolean EsDeterminista(Automata automata)
 	{
-		return false;
+		System.out.println(" >> EsDeterminista ");
+		 
+		boolean esAutomataDeterminista = true;
+		
+		// 
+		 ArrayList<Estado> estados = new ArrayList<Estado>();
+		 int i = 0; 
+		 int y = 0 ;
+		 int tamanoAlfabeto = automata.DameTamanoAlfateto();
+		 estados = automata.DameEstados();
+		 
+		 System.out.println("tama;o alfabeto " +tamanoAlfabeto);
+		 
+		 // ya que se obtiene la lista de estados,
+		 // ahora hay que verificar que cada estado cumpla
+		 // con las reglas de un automata determinista
+		 
+		 System.out.println(estados.size());
+		 
+		 for (i=0; i<estados.size(); i++)
+		 {
+			 
+			// System.out.println(i);
+			 
+			 //
+			 // TempEstimuloTrans es una variable que va ir guardando los simbolos
+			 // utilizados en las transiciones , para identificar SI existen
+			 // mas de una transicion con el mismo estimulo (simbolo)
+			 //
+			 ArrayList<String> tempEstimuloTrans = new ArrayList <String>();
+			 
+			 Estado state = estados.get(i);
+			 
+			 //
+			 // Cada estado tiene sus transiciones.. hay que verificar si cumplen con las
+			 // reglas establecidas
+			 // las siguientes lineas tendran que ser cambiadas a funciones independientes.
+			 //
+			 
+			 ArrayList <Transicion> transiciones = new ArrayList<Transicion>();
+			 transiciones = state.dameTransiciones();
+			 
+			 System.out.println(state.dameNombre() + ": " );
+			 
+			 for( y = 0; y< transiciones.size(); y++)
+			 {
+				 Transicion singleTransicion = transiciones.get(y);
+				 String estimulo = singleTransicion.dameEstimulo();
+				 
+				 System.out.println(singleTransicion.dameEstimulo());
+				 
+				 
+				 
+				 //
+				 // si alguna de las transiciones es vacia "E"
+				 // automaticamente el automata es NO determinista
+				 //
+				 if (estimulo=="E" || estimulo =="e")
+				 {
+					 //
+					 // 
+					 //
+					 esAutomataDeterminista = false;
+					 System.out.println("Este automata tiene una transicion E ... por lo tanto es No determinista ");
+					 break;
+				 }
+				 
+				 //
+				 // Ahora hay que checar si se repite la transicion... 
+				 // si es asi, quiere decir que hubo otra transicion previamente
+				 //
+				 if ( tempEstimuloTrans.contains(estimulo) )
+				 {
+					 esAutomataDeterminista = false;
+					 System.out.println("Este automata tiene dos transiciones del estimulo  " + estimulo );
+					 break;
+				 
+				 }
+				 else
+				 {
+					 tempEstimuloTrans.add(estimulo);
+				 }
+				
+			 }
+			 
+			 //
+			 // Checar si las transiciones de este estado son MENORES al tama;o
+			 // del alfabeto. Esto quisiera decir que el automata no
+			 // cumple con la regla de que cada estado debe poder satisfacer a cada
+			 // simbolo del alfabeto
+ 			 //
+			 if (y < tamanoAlfabeto)
+			 {
+				 System.out.println("Estado " + state.dameNombre() + " solamente satisface " + y + " simbolo");
+				 esAutomataDeterminista = false;
+			 }
+			
+			 
+			 //
+			 // si ya se encontro alguna razon por ser NO determinista.. no hay necesidad
+			 // seguir recorriendo el automata... 
+ 			 //
+			 if (esAutomataDeterminista == false)
+			 {
+				 break;
+			 }
+			 
+		 }
+		
+		 System.out.println("<< EsDeterminista");
+		 
+		 return esAutomataDeterminista ; 
 	}
 }
--- trunk/AutomatonMinimizer/src/itesm/fundamentos/minimizer/vista/AutomatonMinimizer.java
+++ trunk/AutomatonMinimizer/src/itesm/fundamentos/minimizer/vista/AutomatonMinimizer.java
@@ -18,21 +18,38 @@ public class AutomatonMinimizer {
 	    Estado q0 = new Estado("q0");
 	    Estado q1 = new Estado("q1");
 	    Estado q2 = new Estado("q2");
+	    Estado z = new Estado("z");
 	    q0.agregaTransicion(new Transicion("a", q1));
+	    q0.agregaTransicion(new Transicion("b", z));
+	   // q0.agregaTransicion(new Transicion("a", q1));
+	   // q0.agregaTransicion(new Transicion("b", q1));
+	    
 	    q1.agregaTransicion(new Transicion("b", q2));
-	    q2.agregaTransicion(new Transicion("ab", q2));
+	    q1.agregaTransicion(new Transicion("a", z));
+	    q2.agregaTransicion(new Transicion("a", q2));
+	    q2.agregaTransicion(new Transicion("b", q2));
+	    
+	    z.agregaTransicion(new Transicion("a", z));
+	    z.agregaTransicion(new Transicion("b", z));
 	    
 	    //Listo ya generamos las conexiones, ahora a generar el automata
 	    estados.add(q0);
 	    estados.add(q1);
 	    estados.add(q2);
+	    estados.add(z);
 	    
 	    ArrayList<String> alfabeto = new ArrayList<String>();
 	    alfabeto.add("a");
 	    alfabeto.add("b");
 	    
+	    // RJUA
+	    // TODO. Crear una funcion cargarAutomata la cual cargara
+	    // los valores de automatas desde un archivo y creara el objeto
+	    // en tiempo de ejecucion.
+	    //
 	    
-	    Automata automata = new Automata(estados, alfabeto, q0,q1);
+	    
+	    Automata automata = new Automata(estados, alfabeto, q0,q2);
 	    System.out.println("Automata construido!...");
 	    
 	  //---------------FIN CONSTRUCCION DEL AUTOMATA--------//
@@ -41,7 +58,14 @@ public class AutomatonMinimizer {
 	    boolean esAFD = Clasificador.EsDeterminista(automata);
 	    //2.- Si no es AFD tenemos que convertirlo
 	    if(!esAFD)
+	    {	
+	    	System.out.println(" El automata es NO determinista ");
 	    	automata = Convertidor.ConvierteloADeterminista(automata);
+	    }
+	    else
+	    {
+	    	System.out.println(" El automata es determinista ");
+	    }
 	    //3.-Ya convertido a AFD tenemos que minimizarlo
 	    automata = Minimizador.Minimiza(automata);
 	    
